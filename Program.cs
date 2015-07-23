75fdd498a413733616a1a288b0a70e1c54f264fc
avarisc <avarisc@gmail.com>
2015-07-23 04:03:32 -0800

    Clean up output modules and directories

--- Program.cs
+++ Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OpenGame.exe
+namespace OpenGame
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Setup DLL resolution
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.AssemblyResolve += new ResolveEventHandler(ResolveDLL);
+        }
+
+        static Assembly ResolveDLL(object sender, ResolveEventArgs args)
+        {
+            string folderPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string assemblyPath = Path.Combine(folderPath + @"\System\", new AssemblyName(args.Name).Name + ".dll");
+            if (File.Exists(assemblyPath) == false) return null;
+            Assembly assembly = Assembly.LoadFrom(assemblyPath);
+            return assembly;
         }
     }
 }
--- Tools/build-deps.bat
+++ Tools/build-deps.bat
@@ -27,5 +27,19 @@
 	copy %solutiondir%lib\OpenTK\Binaries\OpenTK\Debug\OpenTK.dll %solutiondir%lib\bin\Debug\OpenTK.dll
 	@echo.
 )
+@if not exist %solutiondir%\bin\ mkdir %solutiondir%\bin\
+@if not exist %solutiondir%\bin\Debug mkdir %solutiondir%\bin\Debug
+@if not exist %solutiondir%\bin\Debug\System mkdir %solutiondir%\bin\Debug\System
+@if not exist %solutiondir%\bin\Release\System mkdir %solutiondir%\bin\Release\System
+copy %solutiondir%lib\bin\Release\IronRuby.dll %solutiondir%\bin\Release\System\IronRuby.dll
+copy %solutiondir%lib\bin\Release\IronRuby.dll %solutiondir%\bin\Debug\System\IronRuby.dll
+copy %solutiondir%lib\bin\Release\IronRuby.Libraries.dll %solutiondir%\bin\Release\System\IronRuby.Libraries.dll
+copy %solutiondir%lib\bin\Release\IronRuby.Libraries.dll %solutiondir%\bin\Debug\System\IronRuby.Libraries.dll
+copy %solutiondir%lib\bin\Release\Microsoft.Dynamic.dll %solutiondir%\bin\Release\System\Microsoft.Dynamic.dll
+copy %solutiondir%lib\bin\Release\Microsoft.Dynamic.dll %solutiondir%\bin\Debug\System\Microsoft.Dynamic.dll
+copy %solutiondir%lib\bin\Release\Microsoft.Scripting.dll %solutiondir%\bin\Release\System\Microsoft.Scripting.dll
+copy %solutiondir%lib\bin\Release\Microsoft.Scripting.dll %solutiondir%\bin\Debug\System\Microsoft.Scripting.dll
+copy %solutiondir%lib\bin\Release\OpenTK.dll %solutiondir%\bin\Release\System\OpenTK.dll
+copy %solutiondir%lib\bin\Debug\OpenTK.dll %solutiondir%\bin\Debug\System\OpenTK.dll
 @echo Dependency check finished.
 @echo.
