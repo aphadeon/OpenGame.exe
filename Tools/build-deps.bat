a564160b947171616690099b66864d5c331af3e7
avarisc <avarisc@gmail.com>
2015-07-25 01:04:18 -0800

    Moving OpenTK to NuGet

--- Tools/build-deps.bat
+++ Tools/build-deps.bat
@@ -13,22 +13,9 @@
 	copy %solutiondir%lib\IronLanguages\bin\Release\Microsoft.Scripting.dll %solutiondir%lib\bin\Release\Microsoft.Scripting.dll
 	@echo.
 )
-@if not exist %solutiondir%lib\bin\Release\OpenTK.dll (
-	@echo Building OpenTK Release
-	@echo.
-	%msbuildbinpath%\msbuild %solutiondir%\lib\OpenTK\OpenTK.sln /p:Configuration=Release /verbosity:m /property:TargetFrameworkVersion=4.0,DelaySign=false
-	copy %solutiondir%lib\OpenTK\Binaries\OpenTK\Release\OpenTK.dll %solutiondir%lib\bin\Release\OpenTK.dll
-	@echo.
-)
-@if not exist %solutiondir%lib\bin\Debug\OpenTK.dll (
-	@echo Building OpenTK Debug
-	@echo.
-	%msbuildbinpath%\msbuild %solutiondir%\lib\OpenTK\OpenTK.sln /p:Configuration=Debug /verbosity:m /property:TargetFrameworkVersion=4.0,DelaySign=false
-	copy %solutiondir%lib\OpenTK\Binaries\OpenTK\Debug\OpenTK.dll %solutiondir%lib\bin\Debug\OpenTK.dll
-	@echo.
-)
 @if not exist %solutiondir%\bin\ mkdir %solutiondir%\bin\
 @if not exist %solutiondir%\bin\Debug mkdir %solutiondir%\bin\Debug
+@if not exist %solutiondir%\bin\Release mkdir %solutiondir%\bin\Release
 @if not exist %solutiondir%\bin\Debug\System mkdir %solutiondir%\bin\Debug\System
 @if not exist %solutiondir%\bin\Release\System mkdir %solutiondir%\bin\Release\System
 copy %solutiondir%lib\bin\Release\IronRuby.dll %solutiondir%\bin\Release\System\IronRuby.dll
@@ -39,7 +26,5 @@ copy %solutiondir%lib\bin\Release\Microsoft.Dynamic.dll %solutiondir%\bin\Releas
 copy %solutiondir%lib\bin\Release\Microsoft.Dynamic.dll %solutiondir%\bin\Debug\System\Microsoft.Dynamic.dll
 copy %solutiondir%lib\bin\Release\Microsoft.Scripting.dll %solutiondir%\bin\Release\System\Microsoft.Scripting.dll
 copy %solutiondir%lib\bin\Release\Microsoft.Scripting.dll %solutiondir%\bin\Debug\System\Microsoft.Scripting.dll
-copy %solutiondir%lib\bin\Release\OpenTK.dll %solutiondir%\bin\Release\System\OpenTK.dll
-copy %solutiondir%lib\bin\Debug\OpenTK.dll %solutiondir%\bin\Debug\System\OpenTK.dll
 @echo Dependency check finished.
 @echo.
